b3e005688250ac27c0c7473b208eceb2765fce1a
Christopher Williams <chrsw49@gmail.com>
2020-03-17 01:17:10 -0700

    new xcode proj

---
+++ ARBouncingBallCube/ARBouncingBallCube/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  ARBouncingBallCube
+//
+//  Created by Christopher Williams on 3/17/20.
+//  Copyright © 2020 Christopher Williams. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
